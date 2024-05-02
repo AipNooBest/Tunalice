1369b700c3c0d7035909715db8add6815faca091
AipNooBest <47401054+AipNooBest@users.noreply.github.com>
2024-05-02 20:12:12 +0300

    refactor: Изменение вывода эндпоинта /theory/{id}

--- backend/src/services/theory.ts
+++ backend/src/services/theory.ts
@@ -1,6 +1,10 @@
 import db from '../utils/postgres'
 import ApiResponse from "../models/ApiResponse";
 import {NotFoundError} from "../exceptions/notFoundError";
+import logger from "../utils/logger";
+import c from "../consts";
+import path from "node:path";
+import fs from "fs";
 
 export default {
     list: async () => {
@@ -10,9 +14,25 @@ export default {
     getById: async (id: number) => {
         const specificTheory = await db.query('SELECT theme, path FROM theories WHERE id = $1', [id])
         if (specificTheory.rows.length === 0) {
-            throw new NotFoundError("Теория с данным ID не найдена")
+            throw new NotFoundError(c.THEORY_NOT_FOUND)
         }
-        return new ApiResponse("Успешно", 200, specificTheory.rows)
+        const { theme, path: taskPath } = specificTheory.rows[0]
+        // На всякий случай проверяем, что путь не содержит лишних символов во избежание Path Traversal
+        const fullPath = path.resolve(`${process.cwd() + process.env.DATA_PATH}/theory/` + taskPath)
+        if (!fullPath.startsWith(path.resolve(`${process.cwd() + process.env.DATA_PATH}/theory/`)) || !path.isAbsolute(fullPath)) {
+            logger.warn(fullPath, "Попытка эксплуатации Path Traversal")
+            throw new NotFoundError(c.THEORY_NOT_FOUND)
+        }
+
+        let fileContent: string
+        try {
+            fileContent = fs.readFileSync(fullPath, {encoding: 'utf-8'})
+        } catch (e) {
+            logger.warn(fullPath, "Файл теории по указанному пути не найден")
+            throw new NotFoundError(c.THEORY_NOT_FOUND)
+        }
+
+        return new ApiResponse("Успешно", 200, {theme, content: fileContent})
     },
     categories: async () => {
         const allCategories = await db.query('SELECT DISTINCT theme FROM theories', [])
